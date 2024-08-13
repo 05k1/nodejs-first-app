2f9ba719e0b86b7c7ba1fc21f26f130569399147
Tran Hau <hautran.01022002@gmail.com>
2024-08-14 03:48:13 +0700

    first commit for project

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const productRouter = require("./modules/product/product.router");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", productRouter);
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
---
+++ modules/product/product.router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const productController = require("./product.controller");
+const prefix = "/products";
+
+router.get(prefix, productController.getAllProducts);
+router.get(`${prefix}/create`, productController.createProduct);
+router.post(`${prefix}/create`, productController.postCreateProduct);
+
+module.exports = router;
